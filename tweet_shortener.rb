64fa2100fc2cfc0f044981f642183185ea1f9a8d
Fillan Lee <fillanlee@aol.com>
2019-02-17 11:16:33 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -61,7 +61,7 @@ def selective_tweet_shortener(tweet)
 end
 
 def shortened_tweet_truncator(tweet)
-  if tweet.length < 140
+  if tweet.length > 140
     word_substituter(tweet)
     tweet[0..140] + "..."
   else 
